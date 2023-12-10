8b95f0e052821356240db1a045da92084bf785a4
W4SDJC <88869639+W4SDJC@users.noreply.github.com>
2023-12-10 23:53:51 +0300

    Update and rename Counter to Counter.py

--- Counter.py
+++ Counter.py
@@ -1,29 +1,23 @@
 import tkinter as tk
-from tkinter import *# type: ignore
+from tkinter import *       # type: ignore
 from tkinter import ttk
-from turtle import bgcolor, right
 
 tk = Tk()
 tk.title("Counter")
+#Blocked window resizing and dark background
 tk.resizable(width=False, height=False)
 tk.configure(bg='#595959')
 ttks = ttk.Style()
 ttks.configure("TButton", background='#595959')
-def Plus5(event=None):
-    num = count.get()
-    ans = int(num) + 5
-    count.delete(0, END)
-    count.insert(0, ans)    # type: ignore
 
 def Plus1(event=None):
     num = count.get()
     x = int(num) + 1
     count.delete(0, END)
     count.insert(0, x)      # type: ignore
-
-def Minus5(event=None):
+def Plus5(event=None):
     num = count.get()
-    ans = int(num) - 5
+    ans = int(num) + 5
     count.delete(0, END)
     count.insert(0, ans)    # type: ignore
 
@@ -32,11 +26,16 @@ def Minus1(event=None):
     x = int(num) - 1
     count.delete(0, END)
     count.insert(0, x)      # type: ignore
+def Minus5(event=None):
+    num = count.get()
+    ans = int(num) - 5
+    count.delete(0, END)
+    count.insert(0, ans)    # type: ignore
+
 def Copy():
     num = count.get()
-    tk.clipboard_Reset()    # type: ignore
+    tk.clipboard_clear()   # type: ignore
     tk.clipboard_append(num)
-
 def Reset():
     count.delete(0, END)
     count.insert(0, '0') 
@@ -45,7 +44,7 @@ def Reset():
 screen_width = tk.winfo_screenwidth()
 screen_height = tk.winfo_screenheight()
 window_width = 220
-window_height = 100
+window_height = 80
 x = (screen_width / 2) - (window_width / 2)
 y = (screen_height / 2) - (window_height / 2)
 tk.geometry('%dx%d+%d+%d' % (window_width, window_height, x, y))
@@ -53,34 +52,31 @@ tk.geometry('%dx%d+%d+%d' % (window_width, window_height, x, y))
 lbl = Label(tk, text = "Counter", background='#595959', foreground='white')
 lbl.pack(side=TOP)
 
+#Center buttons
 fcenter = Frame(tk)
-
 count = ttk.Entry(fcenter, width=15)
 count.insert(END,'0')
 
-minus = ttk.Button(fcenter, text = "-", width=3,style="C.TButton", command=lambda:Minus1)
+minus1 = ttk.Button(fcenter, text = "-", width=3,style="C.TButton", command= Minus1)
 tk.bind('-', Minus1)
-
-minus5 = ttk.Button(fcenter, text = "-5", width=3, command=lambda:Minus5)
+minus5 = ttk.Button(fcenter, text = "-5", width=3, command = Minus5)
 tk.bind('<Control-minus>', Minus5)
-
-plus = ttk.Button(fcenter, text="+", width=3, command = lambda:Plus1)
+plus1 = ttk.Button(fcenter, text="+", width=3, command = Plus1)
 tk.bind('+', Plus1)
-
-plus5 = ttk.Button(fcenter, text="+5", width=3, command = lambda:Plus5)
+plus5 = ttk.Button(fcenter, text="+5", width=3, command = Plus5)
 tk.bind('<Control-+>', Plus5)
 
+#Bottom buttons
 fbottom = Frame(tk)
 copy = ttk.Button(fbottom, text= "Copy", command=lambda:Copy())
 reset = ttk.Button(fbottom, text= "Reset", command=lambda:Reset())
 
-colored_btn = ttk.Button(text="Test", style="C.TButton").pack()
-
+#Displaying buttons
 fcenter.pack()
 minus5.pack(side=LEFT)
-minus.pack(side=LEFT)
+minus1.pack(side=LEFT)
 count.pack(side=LEFT, fill=X, expand=True)
-plus.pack(side=LEFT)
+plus1.pack(side=LEFT)
 plus5.pack(side=LEFT)
 
 fbottom.pack()
